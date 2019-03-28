35a1fa1af783cbed9614b3112b7a6fd276bc2ced
Silas Groskreutz <silasgdev@gmail.com>
2019-03-28 04:08:42 -0500

    Updated code to complete for loop to go through sheet and create a new copy

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-# Comment line
+# Comment line @ 4:00:00
 import openpyxl as xl
 wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
 cell = sheet['a1']
 cell = sheet.cell(1, 1)
-print(cell.value)
+
+for row in range(2, sheet.max_row + 1):
+    cell= sheet.cell(row, 3)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transactions2.xlsx')
